79527b80d80328391a952758cf28fdc58dd87805
kim <82406962+KristinaKim6686@users.noreply.github.com>
2021-10-26 22:20:06 +0300

    moved refs to the constructor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 class CountdownTimer {
-  constructor({ selector, targetDate }) {
+  constructor({ selector, targetDate, getRefs }) {
     this.selector = selector;
     this.targetDate = targetDate;
-  }
-  getRefs = () => {
+
+    this.getRefs = () => {
     const container = document.querySelector(this.selector);
     const days = container.querySelector('[data-value="days"]');
     const hours = container.querySelector('[data-value="hours"]');
@@ -15,6 +15,8 @@ class CountdownTimer {
       mins,
       secs
     };
+  }
+  
   };
   setTime = ({ days, hours, mins, secs }) => {
     const time = this.targetDate - Date.now();
@@ -50,53 +52,3 @@ const timer2=new CountdownTimer({
 
 
 
-// class CountdownTimer {
-//   constructor({ selector, targetDate }) {
-//     this.selector = selector;
-//     this.targetDate = targetDate;
-//   }
-//   getRefs = () => {
-//     const container = document.querySelector(this.selector);
-//     const days = container.querySelector('[data-value="days"]');
-//     const hours = container.querySelector('[data-value="hours"]');
-//     const mins = container.querySelector('[data-value="mins"]');
-//     const secs = container.querySelector('[data-value="secs"]');
-//     return {
-//       days,
-//       hours,
-//       mins,
-//       secs,
-//     };
-//   };
-//   setTime = ({ days, hours, mins, secs }) => {
-//     const time = this.targetDate - Date.now();
-//     days.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-//     hours.textContent = this.pad(
-//       Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-//     );
-//     mins.textContent = this.pad(
-//       Math.floor((time % (1000 * 60 * 60)) / (1000 * 60))
-//     );
-//     secs.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-//   };
-//   pad(value) {
-//     return String(value).padStart(2, "0");
-//   }
-//   start = () => {
-//     setInterval(() => {
-//       this.setTime(this.getRefs())
-//     })
-//   }
-// }
-
-// const timer = new CountdownTimer({
-//   selector: "#timer-1",
-//   targetDate: new Date("Nov 17, 2021"),
-// });
-
-// timer.start();
-
-// const timer2 = new CountdownTimer({
-//   selector: "#timer-2",
-//   targetDate: new Date("Nov 25, 2021"),
-// });
